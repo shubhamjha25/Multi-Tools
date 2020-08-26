cce3648519fb6024a419fba2436aa2cfc396f0fe
Soham Das <majumder70.ranu@gmail.com>
2020-08-26 23:56:46 +0530

    Temporary Clock

--- src/Components/Widgets/Clock/clock.js
+++ src/Components/Widgets/Clock/clock.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './clock.css'
 
 const Clock = () => {
-    return (
-        <div>
-            <h2>Clock</h2>
-        </div>
-    );
+    let time = new Date().toLocaleTimeString();
+    const [time1, time2] = useState(time);
+    const UpdateTime = () => {
+        time = new Date().toLocaleTimeString();
+        time2(time);
+    }
+    setInterval(UpdateTime,1000)
+    return(
+        <>
+        <div className="con"></div>
+            <div className="container">
+                <h2>{time1}</h2>
+            </div>
+        </>
+    )
 }
 
 export default Clock;
